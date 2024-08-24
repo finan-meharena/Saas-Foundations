559461c22a3b923465a4a4651788fc07f00e50de
Finan Meharena <finanmeharenakidane@gmail.com>
2024-08-24 12:13:58 -0700

    moved project to new repository

---
+++ src/home/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from visits.models import PageVisit
+
+
+def home_page_view(request, *args, **kwargs ):
+    PageVisit.objects.create()
+    queryset = PageVisit.objects.all()
+    
+    context = {
+        "title" : "Hello finan",
+        "page_visit_count": queryset.count() 
+    }
+
+    return render(request, 'home.html', context=context)
